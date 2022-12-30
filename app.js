5a38b7b197356854d7dbed500a3c044732098e74
Satyam <satus2311@gmail.com>
2022-12-30 11:09:37 +0530

    Vercel compatibility

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const expressValidator = require("express-validator");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+require('dotenv').config();
 // import routes
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const categoryRoutes = require("./routes/category");
-const productRoutes = require("./routes/product");
-const braintreeRoutes = require("./routes/braintree");
-const orderRoutes = require("./routes/order");
-const couponRoutes = require("./routes/couponRoutes");
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree');
+const orderRoutes = require('./routes/order');
+const couponRoutes = require('./routes/couponRoutes');
 
 // app
 const app = express();
@@ -25,30 +25,32 @@ mongoose
     useCreateIndex: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB Connected"));
+  .then(() => console.log('DB Connected'));
 
 // middlewares
-app.get("/", (req, res) => {
-  res.send("Team Lukjury API is running....");
+app.get('/', (req, res) => {
+  res.send('Team Lukjury API is running....');
 });
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
 
 // routes middleware
-app.use("/api", authRoutes);
-app.use("/api", userRoutes);
-app.use("/api", categoryRoutes);
-app.use("/api", productRoutes);
-app.use("/api", braintreeRoutes);
-app.use("/api", orderRoutes);
-app.use("/api", couponRoutes);
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+app.use('/api', braintreeRoutes);
+app.use('/api', orderRoutes);
+app.use('/api', couponRoutes);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
